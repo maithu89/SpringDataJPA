8e6b7b0df7e90bb72040c1e333a1c7356d7ec8d3
minhthu89 <minhthu08092019@gmail.com>
2021-09-12 22:35:17 +0700

    first commit

---
+++ src/main/java/com/example/sringdatajpa/SringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sringdatajpa/SringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
